4c9c1e117a47216acaf65e9e489df4c319a12c43
Andres Montes <hypnoz@live.com>
2023-03-28 22:41:30 -0500

    ajustes menores

--- assets/js/controllers/product-controller.js
+++ assets/js/controllers/product-controller.js
@@ -1,8 +1,8 @@
 import { productService } from "../services/product-service.js";
 
-const productosContainer = document.querySelector("[data-articulos]");
-
 const mostrarProductos = () => {
+    const productosContainer = document.querySelector("[data-articulos]");
+
     productService.listaProductos().then(function(listaProductos){
         listaProductos.forEach(function(producto){
             const productoImagen = producto.imagen;
@@ -26,4 +26,80 @@ const mostrarProductos = () => {
     }); 
 };
 
-mostrarProductos();
+const crearProducto = () =>{
+    const agregarProductoForm = document.querySelector("[data-form]");
+    const seleccionarImg = document.querySelector("[data-imagen]");
+
+    seleccionarImg.addEventListener("change", (evento)=>{
+        const img = evento.target.files[0];
+        const reader = new FileReader();
+
+        reader.readAsDataURL(img);
+        reader.onload = () => {
+            const rutaImg = reader.result;
+            document.querySelector("[data-vistaprevia]").src = rutaImg;
+        };
+    });
+
+    agregarProductoForm.addEventListener("submit", (evento) => {
+        evento.preventDefault();
+
+        /* const urlImagen = document.querySelector("[data-vistaprevia]").src;
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+
+        xhr.onload = function () {
+            const blobImagen = xhr.response;
+            const nombreArchivo = 'imagen.jpg';
+            const rutaCarpeta = '/assets/img/';
+        
+            const formData = new FormData();
+            formData.append('archivo', blobImagen, nombreArchivo);
+        
+            const xhrSubir = new XMLHttpRequest();
+            xhrSubir.open('POST', rutaCarpeta, true);
+            xhrSubir.onload = function () {
+                console.log('Imagen guardada correctamente');
+            };
+            xhrSubir.send(formData);
+        };
+        
+        xhr.open('GET', urlImagen, true);
+        xhr.send();
+
+        const express = require('express');
+        const fs = require('fs');
+        const app = express();
+
+        app.post('/ruta-de-tu-servidor', (req, res) => {
+            const archivo = req.files.archivo;
+            const ruta = '/assets/img/' + archivo.name;
+
+            archivo.mv(ruta, (err) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('Error al guardar el archivo');
+                } else {
+                    console.log('Archivo guardado correctamente');
+                    res.send('Archivo guardado correctamente');
+                }
+            });
+        }); */
+
+        const imagen = document.querySelector("[data-imagen]").value;
+        const categoria = document.querySelector("[data-categoria]").value;
+        const nombre = document.querySelector("[data-nombre]").value;
+        const precio = document.querySelector("[data-precio]").value;
+        const descripcion = document.querySelector("[data-descripcion]").value;
+    
+        productService.agregarProducto(imagen, categoria, nombre, precio, descripcion);
+    });
+};
+
+if(window.location.pathname == "/assets/templates/productos.html"){
+    mostrarProductos();
+};
+
+if(window.location.pathname == "/assets/templates/agregar_producto.html"){
+    crearProducto();
+}
--- assets/js/services/product-service.js
+++ assets/js/services/product-service.js
@@ -2,6 +2,32 @@ const listaProductos = () => {
     return fetch(`http://localhost:3000/producto`).then(respuesta => respuesta.json())
 };
 
+const agregarProducto = (imagen, categoria, nombre, precio, descripcion) => {
+    return fetch(`http://localhost:3000/producto`, {
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({id: uuid.v4(), imagen, categoria, nombre, precio, descripcion})
+    });
+};
+
+const cargarImagen = (formData) => {
+    return fetch("http://localhost:3000/producto",{
+        method: "POST",
+        body: formData
+    })
+    .then(respuesta => respuesta.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+};
+
 export const productService = {
     listaProductos,
+    cargarImagen,
+    agregarProducto,
 };
